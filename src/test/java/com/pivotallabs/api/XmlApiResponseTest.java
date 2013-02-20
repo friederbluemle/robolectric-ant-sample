7fae5eef2e6b7e9732a5182628892e581daa9f05
Lilibeth De La Cruz and Phil Goodwin <pair+lilibeth+phil@pivotallabs.com>
2013-02-20 15:38:51 -0800

    Created ApiResponseTest

--- src/test/java/com/pivotallabs/api/XmlApiResponseTest.java
+++ src/test/java/com/pivotallabs/api/XmlApiResponseTest.java
@@ -4,32 +4,12 @@ import com.pivotallabs.TestResponses;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
-import static com.pivotallabs.TestResponses.GENERIC_XML;
 import static com.pivotallabs.api.Xmls.getTextContentOfChild;
 import static com.pivotallabs.util.Strings.asStream;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class XmlApiResponseTest {
-
-    @Test
-    public void isSuccess_shouldReturnTrueIfResponseCodeIsInThe200Range() throws Exception {
-        assertThat(new XmlApiResponse(200, asStream(GENERIC_XML)).isSuccess(), equalTo(true));
-        assertThat(new XmlApiResponse(201, asStream(GENERIC_XML)).isSuccess(), equalTo(true));
-        assertThat(new XmlApiResponse(299, asStream(GENERIC_XML)).isSuccess(), equalTo(true));
-    }
-
-    @Test
-    public void isSuccess_shouldReturnFalseIfResponseCodeIsIn500Range() throws Exception {
-        assertThat(new XmlApiResponse(500, asStream(GENERIC_XML)).isSuccess(), equalTo(false));
-        assertThat(new XmlApiResponse(501, asStream(GENERIC_XML)).isSuccess(), equalTo(false));
-    }
-
-    @Test
-    public void isUnauthorized_shouldReturnTrueIfResponseCodeIs401() throws Exception {
-        assertThat(new XmlApiResponse(401, asStream("Access Denied")).isUnauthorized(), equalTo(true));
-    }
-
     @Test
     public void parseResponse_shouldAssignAnXmlDocumentFromTheResponseBody() throws Exception {
         XmlApiResponse apiResponse = new XmlApiResponse(200, asStream(TestResponses.AUTH_SUCCESS));
